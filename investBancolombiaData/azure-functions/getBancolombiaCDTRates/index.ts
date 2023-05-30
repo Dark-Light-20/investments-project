8dfe8b9c00b9627a0cdd69f1e01697c2b474010e
Dark-Light-20 <pazsebas@hotmail.com>
2023-05-29 23:30:57 -0500

    fix: Fixed content type header for Azure function (getBancolombiaCDTRates)

--- investBancolombiaData/azure-functions/getBancolombiaCDTRates/index.ts
+++ investBancolombiaData/azure-functions/getBancolombiaCDTRates/index.ts
@@ -6,7 +6,6 @@ const httpTrigger: AzureFunction = async function (
   context: Context,
   req: HttpRequest
 ): Promise<void> {
-  context.res.headers = { "Content-Type": "application/json" };
   try {
     const rates: CDTRange[] = await getCDTRates();
     context.res = { body: rates };
@@ -14,6 +13,7 @@ const httpTrigger: AzureFunction = async function (
     const { message } = error as Error;
     context.res = { status: 500, body: message };
   }
+  context.res.headers = { "Content-Type": "application/json" };
 };
 
 export default httpTrigger;
