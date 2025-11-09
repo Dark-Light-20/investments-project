f6235fe27184c189f03e35f1b6b9097c2a56c6bd
Dark Light <pazsebas@hotmail.com>
2025-11-09 01:19:06 -0500

    fix(invest-ban100): :bug: Fixed cdt rates terms calculation logic

--- invest-ban100/azure-functions/src/infrastructure/mappers/cdt.mapper.ts
+++ invest-ban100/azure-functions/src/infrastructure/mappers/cdt.mapper.ts
@@ -1,15 +1,20 @@
 import { CdtRS } from "../models/cdt.model.js";
 import { CDTRate, CDTTermUnit } from "@dark-light-20/invest-domain";
+import {
+  LIMIT_INVESTMENT_TERM,
+  MAXIMUM_CDT_AMOUNT,
+  MINIMUM_CDT_AMOUNT,
+} from "../utils/number.constants.js";
 
 export const cdtMapper = (response: CdtRS[]): CDTRate[] =>
   response.map((item, index) => ({
     rate: Number.parseFloat(item.field_tasa_interes),
-    minimumAmount: 250000,
-    minimumTerm:
-      index === 0
-        ? 0
-        : Number.parseInt(/\d+/.exec(response[index - 1].name)![0]) + 1,
-    maximumAmount: 999999999999,
-    maximumTerm: Number.parseInt(/\d+/.exec(item.name)![0]),
+    minimumAmount: MINIMUM_CDT_AMOUNT,
+    minimumTerm: Number.parseInt(/\d+/.exec(item.name)![0]),
+    maximumAmount: MAXIMUM_CDT_AMOUNT,
+    maximumTerm:
+      index === response.length - 1
+        ? LIMIT_INVESTMENT_TERM
+        : Number.parseInt(/\d+/.exec(response[index + 1].name)![0]) - 1,
     termUnit: CDTTermUnit.DAYS,
   }));
