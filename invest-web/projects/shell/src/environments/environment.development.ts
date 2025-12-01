8be8a609b995a2c6a56eec7832b849f732eeaf14
Dark Light <pazsebas@hotmail.com>
2025-12-01 16:48:12 -0500

    ci(shell): :construction_worker: Added variable for manifest config

--- invest-web/projects/shell/src/environments/environment.development.ts
+++ invest-web/projects/shell/src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
   assetsUrl: 'http://localhost:4200',
+  manifestUrl: 'http://localhost:4200/federation-local.manifest.json',
 };
--- invest-web/projects/shell/src/main.ts
+++ invest-web/projects/shell/src/main.ts
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { initFederation } from '@angular-architects/native-federation';
+import { environment } from './environments/environment';
 
-initFederation('federation.manifest.json')
+initFederation(environment.manifestUrl)
   .catch(err => console.error(err))
   .then(_ => import('./bootstrap'))
   .catch(err => console.error(err));
