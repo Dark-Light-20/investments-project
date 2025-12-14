8a8b77d75a0e46dd3fb3c1159459086acbbc9efe
Dark Light <pazsebas@hotmail.com>
2025-12-14 00:25:07 -0500

    refactor(invest-finandina): :recycle: Upgrade invest-domain dependency to version 1.0.0 with new simulation logic and refactor endpoints for GET requests

--- invest-finandina/azure-functions/src/functions/getPocketSimulation.ts
+++ invest-finandina/azure-functions/src/functions/getPocketSimulation.ts
@@ -1,29 +1,29 @@
 import { app, HttpRequest, HttpResponseInit } from "@azure/functions";
 import { PocketUseCase } from "@dark-light-20/invest-domain/dist/application/use-cases/pocket.use-case.js";
-import { BoxInvestRQ } from "./models/cdt-rq.model.js";
 import { PocketService } from "../infrastructure/http-adapter/pocket.service.js";
 
-export async function calculatePocketInvest(
+export async function getPocketSimulation(
   request: HttpRequest
 ): Promise<HttpResponseInit> {
   try {
     const pocketUseCase = new PocketUseCase(new PocketService());
-    const { amount, months, monthlyIncrement } =
-      (await request.json()) as BoxInvestRQ;
-    const invest = await pocketUseCase.calculateInvest(
+    const amount = Number(request.query.get("amount"));
+    const months = Number(request.query.get("months"));
+    const monthlyIncrement = Number(request.query.get("monthlyIncrement"));
+    const simulation = await pocketUseCase.simulateInvest(
       amount,
       months,
-      monthlyIncrement
+      monthlyIncrement || undefined
     );
-    return { jsonBody: invest.toFixed(2) };
+    return { jsonBody: simulation };
   } catch (error) {
     const { message } = error as Error;
     return { status: 400, body: message };
   }
 }
 
-app.http("calculatePocketInvest", {
-  methods: ["POST"],
+app.http("getPocketSimulation", {
+  methods: ["GET"],
   authLevel: "anonymous",
-  handler: calculatePocketInvest,
+  handler: getPocketSimulation,
 });
